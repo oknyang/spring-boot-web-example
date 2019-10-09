cf6da048ff45f7966899cc978942d7fc5de4ccff
oknyang <lo4ol@naver.com>
2019-10-09 23:53:01 +0900

    게시글 조회 페이지

--- src/main/java/com/springboot/community/web/WebApplication.java
+++ src/main/java/com/springboot/community/web/WebApplication.java
@@ -1,13 +1,46 @@
 package com.springboot.community.web;
 
+import com.springboot.community.web.domain.Board;
+import com.springboot.community.web.domain.User;
+import com.springboot.community.web.domain.enums.BoardType;
+import com.springboot.community.web.repository.BoardRepository;
+import com.springboot.community.web.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
+        return (args) -> {
+            User user = userRepository.save(User.builder()
+                    .name("havi")
+                    .password("test")
+                    .email("havi@gmail.com")
+                    .createdDate(LocalDateTime.now())
+                    .build());
+
+            IntStream.rangeClosed(1, 200).forEach(index ->
+                    boardRepository.save(Board.builder()
+                            .title("게시글" + index)
+                            .subTitle("순서" + index)
+                            .content("콘텐츠")
+                            .boardType(BoardType.free)
+                            .createdDate(LocalDateTime.now())
+                            .updatedDate(LocalDateTime.now())
+                            .user(user)
+                            .build()));
+        };
     }
 
 }
