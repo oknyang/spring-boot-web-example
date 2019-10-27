6ed7a3c8de4e07c36c54a9e5cc715254876302dd
oknyang <lo4ol@naver.com>
2019-10-27 22:10:40 +0900

    oauth 설정

---
+++ src/main/java/com/springboot/community/web/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.springboot.community.web.config;
+
+import com.springboot.community.web.oauth.ClientResources;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    @ConfigurationProperties("facebook")
+    public ClientResources facebook() {
+        return new ClientResources();
+    }
+
+    @Bean
+    @ConfigurationProperties("google")
+    public ClientResources google() {
+        return new ClientResources();
+    }
+
+    @Bean
+    @ConfigurationProperties("kakao")
+    public ClientResources kakao() {
+        return new ClientResources();
+    }
+}
